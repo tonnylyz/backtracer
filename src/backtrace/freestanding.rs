acf181ef251cc90d6ef1f483e677cb453a6ca707
tonnylyz <lyztonny@gmail.com>
2021-03-29 14:12:38 +0800

    Adapt to aarch64

--- src/backtrace/freestanding.rs
+++ src/backtrace/freestanding.rs
@@ -1,21 +1,23 @@
 #[derive(Debug, Clone)]
 pub struct Frame {
-    rbp: u64,
-    rsp: u64,
-    rip: u64,
+    fp: u64,
+    sp: u64,
+    pc: u64,
+    lr: u64,
 }
 
 impl Frame {
-    pub fn new(rbp: u64, rsp: u64, rip: u64) -> Frame {
+    pub fn new(fp: u64, sp: u64, pc: u64, lr: u64) -> Frame {
         Frame {
-            rbp: rbp,
-            rsp: rsp,
-            rip: rip,
+            fp,
+            sp,
+            pc,
+            lr,
         }
     }
 
     pub fn ip(&self) -> *mut u8 {
-        (self.rip - 1) as *mut u8
+        self.pc as *mut u8
     }
 
     pub fn symbol_address(&self) -> *mut u8 {
@@ -36,11 +38,10 @@ pub fn trace_from(mut curframe: Frame, cb: &mut dyn FnMut(&super::Frame) -> bool
 
         if keep_going {
             unsafe {
-                curframe.rip = *((curframe.rbp + 8) as *mut u64);
-                curframe.rsp = curframe.rbp;
-                curframe.rbp = *(curframe.rbp as *mut u64);
+                curframe.pc = *((curframe.fp + 8) as *mut u64);
+                curframe.fp = *(curframe.fp as *mut u64);
 
-                if curframe.rip == 0 || curframe.rbp <= 0xfff {
+                if curframe.pc == 0 || curframe.fp <= 0xfff {
                     break;
                 }
             }
@@ -52,7 +53,8 @@ pub fn trace_from(mut curframe: Frame, cb: &mut dyn FnMut(&super::Frame) -> bool
 
 #[inline(always)]
 pub fn trace(cb: &mut dyn FnMut(&super::Frame) -> bool) {
-    use x86::current::registers;
-    let curframe = Frame::new(registers::rbp(), registers::rsp(), registers::rip());
+    use cortex_a::regs::*;
+
+    let curframe = Frame::new(FP.get(), SP.get(), PC.get(), LR.get());
     trace_from(curframe.clone(), cb);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate addr2line;
 extern crate alloc;
 extern crate cfg_if;
 extern crate log;
-extern crate x86;
+extern crate cortex_a;
 
 pub use backtrace::{trace, trace_from, EntryPoint, Frame};
 mod backtrace;
